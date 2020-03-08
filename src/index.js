551eb58e688621e70a88b567811793a2e91c1c07
Dmitiy Glebov <webb17@yandex.ru>
2020-03-08 15:15:43 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr=[];
+  let array=[];
+  let res;
+  for(let i = 0; i < bracketsConfig.length; i++){
+    arr = arr.concat(bracketsConfig[i]);
+  }
+  str.split('').forEach((i,index)=>{
+    array.length != 0 && array[array.length-1] === arr[arr.indexOf(i)+1] ? array.pop() :
+    arr.indexOf(i) % 2 == 0 ? array.push(i) :
+    array[array.length-1] === arr[arr.indexOf(i)-1] ? array.pop() : res = 1; 
+  })
+ 
+    return !res && array.length == 0 ? true : false;
 }
